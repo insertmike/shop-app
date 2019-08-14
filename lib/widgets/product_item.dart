ca0cad0f2b516268fdf84b9a46be57a92283f6a7
Mihail Yonchev <myonchev99@gmail.com>
2019-08-14 17:10:44 +0300

    Added Provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
+  final double price;
 
-  ProductItem({this.id, this.title, this.imageUrl});
+  ProductItem({this.id, this.title, this.imageUrl, this.price});
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black54,
@@ -32,9 +40,22 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {},
           ),
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                title,
+                //    textAlign: TextAlign.center,
+              ),
+              Text(
+                '\$${price.toString()}',
+                style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+                //  textAlign: TextAlign.center,
+              ),
+            ],
           ),
         ),
       ),
