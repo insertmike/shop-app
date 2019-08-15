0a53461fb2e4a64bcba377827379a92903de36ce
Mihail Yonchev <myonchev99@gmail.com>
2019-08-16 01:02:55 +0300

    Added Product Detail Screen body

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -8,14 +8,39 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    // Set listen to false, because we don't want to rebuild widget if another 
+    // Set listen to false, because we don't want to rebuild widget if another
     // product is loaded elsewhere. We need the widget to be built once.
-    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-      body: null,
+      body: SingleChildScrollView(
+              child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text('\$${loadedProduct.price}',
+            style: TextStyle(color: Colors.green, fontSize: 20),),
+            SizedBox(height: 10,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(loadedProduct.description, textAlign: TextAlign.center, 
+              softWrap: true,),
+            )
+
+          ],
+        ),
+      ),
     );
   }
-}//
+} //
