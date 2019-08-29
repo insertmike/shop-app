ff1a18d47e29f06543f8018e0dbfed53e0d9997f
Mihail Yonchev <myonchev99@gmail.com>
2019-08-29 17:06:42 +0300

    Implemented pull to refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import '../screens/edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProds(BuildContext context) async {
+    await Provider.of<Products>(context).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -24,19 +28,22 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: productsData.items.length,
-          itemBuilder: (ctx, index) => Column(
-            children: <Widget>[
-              UserProductItem(
-                id: productsData.items[index].id,
-                imageUrl: productsData.items[index].imageUrl,
-                title: productsData.items[index].title,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProds(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (ctx, index) => Column(
+              children: <Widget>[
+                UserProductItem(
+                  id: productsData.items[index].id,
+                  imageUrl: productsData.items[index].imageUrl,
+                  title: productsData.items[index].title,
+                ),
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
