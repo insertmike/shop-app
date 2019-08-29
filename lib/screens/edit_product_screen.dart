5256bcb74507142f410d0defe838ef1e57260724
Mihail Yonchev <myonchev99@gmail.com>
2019-08-29 17:31:28 +0300

    Added patch requests for updating the data

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -91,11 +91,14 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
+          setState(() {
+          //_isLoading = false;
+          });
+          Navigator.of(context).pop();
+      
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -116,12 +119,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = true;
-        });
-        Navigator.of(context).pop();
       }
+
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.of(context).pop();
     }
   }
 
